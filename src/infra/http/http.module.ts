1f52b191a605f7733662084a902a675db8316a0c
Luis Fernando <luisfernandobr2003@gmail.com>
2024-02-07 13:43:08 -0300

    feat(http): add authenticate controller

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common'
 
+import { AuthenticateStudentUseCase } from '~/domain/forum/application/use-cases/authenticate-student'
 import { RegisterStudentUseCase } from '~/domain/forum/application/use-cases/register-student'
 import { CryptographyModule } from '~/infra/cryptography/cryptography.module'
 import { DatabaseModule } from '~/infra/database/database.module'
+import { AuthenticateController } from '~/infra/http/controllers/authenticate.controller'
 import { CreateAccountController } from '~/infra/http/controllers/create-account.controller'
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
-  controllers: [CreateAccountController],
-  providers: [RegisterStudentUseCase],
+  controllers: [CreateAccountController, AuthenticateController],
+  providers: [RegisterStudentUseCase, AuthenticateStudentUseCase],
 })
 export class HttpModule {}
