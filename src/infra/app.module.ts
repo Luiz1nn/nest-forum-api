04a21afd4e6ddd414af6385a98c7b3cb3324903e
Luis Fernando <luisfernandobr2003@gmail.com>
2024-02-07 13:42:50 -0300

    feat(app): add auth module

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 
+import { AuthModule } from '~/infra/auth/auth.module'
 import { envSchema } from '~/infra/env/env'
 import { EnvModule } from '~/infra/env/env.module'
 import { HttpModule } from '~/infra/http/http.module'
@@ -11,6 +12,7 @@ import { HttpModule } from '~/infra/http/http.module'
       validate: (env) => envSchema.parse(env),
       isGlobal: true,
     }),
+    AuthModule,
     EnvModule,
     HttpModule,
   ],
