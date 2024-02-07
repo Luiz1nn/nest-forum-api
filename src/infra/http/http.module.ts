5f3595e203f982901435d46a9a7ab5bdc1b0fdf5
Luis Fernando <luisfernandobr2003@gmail.com>
2024-02-07 14:49:59 -0300

    feat(http): add create question controller

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,15 +1,25 @@
 import { Module } from '@nestjs/common'
 
 import { AuthenticateStudentUseCase } from '~/domain/forum/application/use-cases/authenticate-student'
+import { CreateQuestionUseCase } from '~/domain/forum/application/use-cases/create-question'
 import { RegisterStudentUseCase } from '~/domain/forum/application/use-cases/register-student'
 import { CryptographyModule } from '~/infra/cryptography/cryptography.module'
 import { DatabaseModule } from '~/infra/database/database.module'
 import { AuthenticateController } from '~/infra/http/controllers/authenticate.controller'
 import { CreateAccountController } from '~/infra/http/controllers/create-account.controller'
+import { CreateQuestionController } from '~/infra/http/controllers/create-question.controller'
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
-  controllers: [CreateAccountController, AuthenticateController],
-  providers: [RegisterStudentUseCase, AuthenticateStudentUseCase],
+  controllers: [
+    CreateAccountController,
+    AuthenticateController,
+    CreateQuestionController,
+  ],
+  providers: [
+    RegisterStudentUseCase,
+    AuthenticateStudentUseCase,
+    CreateQuestionUseCase,
+  ],
 })
 export class HttpModule {}
