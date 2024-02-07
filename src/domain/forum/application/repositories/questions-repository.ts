2b739d7bf7ae206e0b906f9445df0dd30c5c6c52
Luis Fernando <luisfernandobr2003@gmail.com>
2024-02-07 14:54:32 -0300

    feat(forum): add findManyRecent in questions repository

--- src/domain/forum/application/repositories/questions-repository.ts
+++ src/domain/forum/application/repositories/questions-repository.ts
@@ -1,5 +1,7 @@
+import { PaginationParams } from '~/core/repositories/pagination-params'
 import { Question } from '~/domain/forum/enterprise/entities/question'
 
 export abstract class QuestionsRepository {
+  abstract findManyRecent(params: PaginationParams): Promise<Question[]>
   abstract create(question: Question): Promise<void>
 }
