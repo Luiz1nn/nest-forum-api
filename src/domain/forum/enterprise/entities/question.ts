6cc9d2ee422a65d99ef29398817cc93e993ff6e4
Luis Fernando <luisfernandobr2003@gmail.com>
2024-02-07 13:59:12 -0300

    feat(forum): add question entity

---
+++ src/domain/forum/enterprise/entities/question.ts
@@ -0,0 +1,28 @@
+import { AggregateRoot } from '~/core/entities/aggregate-root'
+import { UniqueEntityID } from '~/core/entities/unique-entity-id'
+import { Optional } from '~/core/types/optional'
+
+type QuestionProps = {
+  authorId: UniqueEntityID
+  title: string
+  content: string
+  createdAt: Date
+  updateAt?: Date | null
+}
+
+export class Question extends AggregateRoot<QuestionProps> {
+  static create(
+    props: Optional<QuestionProps, 'createdAt'>,
+    id?: UniqueEntityID,
+  ) {
+    const question = new Question(
+      {
+        ...props,
+        createdAt: props.createdAt ?? new Date(),
+      },
+      id,
+    )
+
+    return question
+  }
+}
