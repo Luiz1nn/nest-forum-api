4981b502e7c475dca53bc86174aa8d05a1441588
Luis Fernando <luisfernandobr2003@gmail.com>
2024-02-15 10:53:42 -0300

    feat(forum): add fetch recent questions use case

---
+++ src/domain/forum/application/use-cases/fetch-recent-questions.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common'
+
+import { Either, right } from '~/core/either'
+import { QuestionsRepository } from '~/domain/forum/application/repositories/questions-repository'
+import { Question } from '~/domain/forum/enterprise/entities/question'
+
+type FetchRecentQuestionsUseCaseRequest = {
+  page: number
+}
+
+type FetchRecentQuestionsUseCaseResponse = Either<
+  null,
+  {
+    questions: Question[]
+  }
+>
+
+@Injectable()
+export class FetchRecentQuestionsUseCase {
+  constructor(private questionsRepository: QuestionsRepository) {}
+
+  async execute({
+    page,
+  }: FetchRecentQuestionsUseCaseRequest): Promise<FetchRecentQuestionsUseCaseResponse> {
+    const questions = await this.questionsRepository.findManyRecent({ page })
+
+    return right({
+      questions,
+    })
+  }
+}
--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -2,7 +2,7 @@ import { AggregateRoot } from '~/core/entities/aggregate-root'
 import { UniqueEntityID } from '~/core/entities/unique-entity-id'
 import { Optional } from '~/core/types/optional'
 
-type QuestionProps = {
+export type QuestionProps = {
   authorId: UniqueEntityID
   title: string
   content: string
