f4caa06df86cd1a93abf1c6fc9e8bab622eaa75f
Luiz1nn <luisfernandobr2003@gmail.com>
2024-02-17 13:20:17 -0300

    feat(http): add edit question controller

--- src/infra/database/prisma/repositories/prisma-questions-repository.ts
+++ src/infra/database/prisma/repositories/prisma-questions-repository.ts
@@ -11,6 +11,18 @@ import { PrismaService } from '../prisma.service'
 export class PrismaQuestionsRepository implements QuestionsRepository {
   constructor(private prisma: PrismaService) {}
 
+  async findById(id: string): Promise<Question | null> {
+    const question = await this.prisma.question.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    if (!question) return null
+
+    return PrismaQuestionMapper.toDomain(question)
+  }
+
   async findManyRecent({ page }: PaginationParams): Promise<Question[]> {
     const questions = await this.prisma.question.findMany({
       orderBy: {
@@ -29,5 +41,18 @@ export class PrismaQuestionsRepository implements QuestionsRepository {
     await this.prisma.question.create({
       data,
     })
+  }
+
+  async save(question: Question): Promise<void> {
+    const data = PrismaQuestionMapper.toPrisma(question)
+
+    await Promise.all([
+      this.prisma.question.update({
+        where: {
+          id: question.id.toString(),
+        },
+        data,
+      }),
+    ])
   }
 }
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common'
 
 import { AuthenticateStudentUseCase } from '~/domain/forum/application/use-cases/authenticate-student'
 import { CreateQuestionUseCase } from '~/domain/forum/application/use-cases/create-question'
+import { EditQuestionUseCase } from '~/domain/forum/application/use-cases/edit-question'
 import { FetchRecentQuestionsUseCase } from '~/domain/forum/application/use-cases/fetch-recent-questions'
 import { RegisterStudentUseCase } from '~/domain/forum/application/use-cases/register-student'
 import { CryptographyModule } from '~/infra/cryptography/cryptography.module'
@@ -9,8 +10,8 @@ import { DatabaseModule } from '~/infra/database/database.module'
 import { AuthenticateController } from '~/infra/http/controllers/authenticate.controller'
 import { CreateAccountController } from '~/infra/http/controllers/create-account.controller'
 import { CreateQuestionController } from '~/infra/http/controllers/create-question.controller'
-
-import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller'
+import { EditQuestionController } from '~/infra/http/controllers/edit-question.controller'
+import { FetchRecentQuestionsController } from '~/infra/http/controllers/fetch-recent-questions.controller'
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
@@ -19,12 +20,14 @@ import { FetchRecentQuestionsController } from './controllers/fetch-recent-quest
     AuthenticateController,
     CreateQuestionController,
     FetchRecentQuestionsController,
+    EditQuestionController,
   ],
   providers: [
     RegisterStudentUseCase,
     AuthenticateStudentUseCase,
     CreateQuestionUseCase,
     FetchRecentQuestionsUseCase,
+    EditQuestionUseCase,
   ],
 })
 export class HttpModule {}
