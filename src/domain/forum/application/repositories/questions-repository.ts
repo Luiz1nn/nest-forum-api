2bc921b26ccd48ac0ffed1feecd8cf1ea0d7dc43
Luiz1nn <luisfernandobr2003@gmail.com>
2024-02-17 12:17:09 -0300

    feat(repositories): update questions repository

--- src/domain/forum/application/repositories/questions-repository.ts
+++ src/domain/forum/application/repositories/questions-repository.ts
@@ -2,6 +2,8 @@ import { PaginationParams } from '~/core/repositories/pagination-params'
 import { Question } from '~/domain/forum/enterprise/entities/question'
 
 export abstract class QuestionsRepository {
+  abstract findById(id: string): Promise<Question | null>
   abstract findManyRecent(params: PaginationParams): Promise<Question[]>
   abstract create(question: Question): Promise<void>
+  abstract save(question: Question): Promise<void>
 }
